309c5a58bdb8afa8ac4e3d85a529275caa0c8a8e
lonelyhope <13719327563@163.com>
2017-10-16 10:12:49 -0500

    Add files via upload

--- selpg.go
+++ selpg.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
-	"strconv"
-	"strings"
 )
 
 /*================================= types =========================*/
@@ -29,21 +28,13 @@ var progname string /* program name, for error messages */
 
 /*================================= main()=== =====================*/
 func main() {
-	args := os.Args
-	progname = args[0]
-	//fmt.Println(args)
+	progname = "selpg"
 	selpgargs := SelpgArgs{StartPage: -1, EndPage: -1, PageLen: 72, Pagetype: 'l', inFilename: ""}
-	processArgs(args, &selpgargs)
+	processArgs(&selpgargs)
 	processInput(selpgargs)
 }
 
 /*================================= process_args() ================*/
-func truncateToInt(s string, start, end int) int {
-	st := s[start:end]
-	t, _ := strconv.Atoi(st)
-	return t
-}
-
 func testStat(filePath string, rw int) int {
 	res := [4]int{0, 0, 0, 0}
 	fileInfo, err := os.Stat(filePath)
@@ -75,130 +66,42 @@ func testStat(filePath string, rw int) int {
 	}
 }
 
-func processArgs(args []string, selpgargs *SelpgArgs) {
-	//fmt.Println("processArgs")
-	var s1, s2 string
-	var argno int
-	var i int
+func processArgs(selpgargs *SelpgArgs) {
+	startPage := flag.Int("s", 0, "the start page")
+	endPage := flag.Int("e", 0, "the start page")
+	pageTypeF := flag.Bool("f", false, "sepearte pages use EOF")
+	pageLen := flag.Int("l", 72, "the length of a page")
+	printdest := flag.String("d", "", "the dest sub program")
+	flag.Parse()
 
-	//fmt.Println(args)
-	argLen := len(args)
-	if argLen < 3 {
-		fmt.Println(progname, ": not enough arguments")
+	if *startPage <= 0 || *endPage <= 0 || *startPage > *endPage {
+		fmt.Println("illegal start or end page number")
 		os.Exit(1)
 	}
-
-	/* handle 1st arg - start page */
-	s1 = args[1]
-	if len(s1) < 3 || strings.Compare(s1[0:2], "-s") != 0 {
-		fmt.Println(progname, ": 1st arg should be start_page")
-		os.Exit(2)
+	selpgargs.StartPage = *startPage
+	selpgargs.EndPage = *endPage
+	selpgargs.PageLen = *pageLen
+	selpgargs.printDest = *printdest
+	if *pageTypeF {
+		selpgargs.Pagetype = 'f'
 	}
-	i = truncateToInt(s1, 2, len(s1))
-	if i < 1 || i > (IntMax-1) {
-		fmt.Println(progname, ": invalid start page")
-		os.Exit(3)
-	}
-	selpgargs.StartPage = i
-
-	/* handle 2nd arg - end page */
-	s1 = args[2]
-	if len(s1) < 3 || strings.Compare(s1[0:2], "-e") != 0 {
-		//fmt.Println(s1)
-		fmt.Println(progname, ": 2nd arg should be -eend_page")
-		os.Exit(4)
-	}
-	i = truncateToInt(s1, 2, len(s1))
-	if (i < 1) || (i > (IntMax-1) || i < selpgargs.StartPage) {
-		fmt.Println(progname, ": invalid end page")
-		os.Exit(5)
-	}
-	selpgargs.EndPage = i
-
-	/* now handle optional args */
-	argno = 3
-	for argno <= (argLen-1) && args[argno][0] == '-' {
-		/* while there more args and they start with a '-' */
-		s1 = args[argno]
-		switch s1[1] {
-		case 'l':
-			s2 = s1[2:len(s1)]
-			i, _ = strconv.Atoi(s2)
-			if i < 1 || i > (IntMax-1) {
-				fmt.Println(progname, ": invalid page length ", s2)
-				os.Exit(6)
-			}
-			selpgargs.PageLen = i
-			selpgargs.Pagetype = 'l'
-			argno = argno + 1
-
-		case 'f':
-			/* check if just "-f" or something more */
-			if strings.Compare(s1, "-f") != 0 {
-				fmt.Println(progname, ": option should be \"-f\"")
-				os.Exit(7)
-			}
-			selpgargs.Pagetype = 'f'
-			argno = argno + 1
-
-		case 'd':
-			s2 = s1[2:len(s1)]
-			if len(s2) < 1 {
-				fmt.Println(progname, ": -d option requires a printer destination")
-				os.Exit(8)
-			}
-			selpgargs.printDest = s2
-			argno = argno + 1
-
-		default:
-			fmt.Println(progname, ": unknown option ", s1)
-			os.Exit(9)
-		} /* end switch */
-	} /* end for */
-
-	/*++argno;*/
-	if argno <= argLen-1 { /* there is one more arg */
-		selpgargs.inFilename = args[argno]
-		//fileInfo, err := os.Stat(selpgargs.inFilename)
-		/* check if file exists */
-		if testStat(selpgargs.inFilename, 1) == -1 {
-			fmt.Println(progname, ": input file  does not exist")
-			os.Exit(10)
-		}
-		/* check if file is readable */
-		if testStat(selpgargs.inFilename, 1) == 0 {
-			fmt.Println(progname, ": input file exists but cannot be read")
-			os.Exit(11)
+	//fmt.Println(flag.Args())
+	if len(flag.Args()) > 0 {
+		selpgargs.inFilename = (flag.Args())[0]
+		if testStat(selpgargs.inFilename, 1) != 1 {
+			fmt.Println("you can't  read the file")
+			os.Exit(2)
 		}
 	}
-	//fmt.Println(selpgargs)
-	if selpgargs.StartPage < 0 {
-		fmt.Println("The Start Page should bigger to 0")
-		os.Exit(12)
-	}
-	if selpgargs.EndPage < 0 || selpgargs.EndPage < selpgargs.StartPage {
-		fmt.Println("the end page should be bigger to start page")
-		os.Exit(13)
-	}
-	if selpgargs.PageLen < 1 {
-		fmt.Println("the page length should be bigger than 0")
-		os.Exit(14)
-	}
-	if selpgargs.Pagetype != 'l' && selpgargs.Pagetype != 'f' {
-		fmt.Println(selpgargs.Pagetype)
-		fmt.Println("illegal page type")
-		os.Exit(15)
-	}
+	//fmt.Println(*selpgargs)
 }
 
 func processInput(selpgargs SelpgArgs) {
-	//fmt.Println("processInput")
-	//fmt.Println(selpgargs)
 	var reader io.Reader
 	var writer io.Writer
 	var err error
 	var EOF rune = 26
-	//Writer := bufio.NewWriter(writer)
+
 	if selpgargs.inFilename == "" {
 		reader = os.Stdin
 	} else {
